2c09c86f3390cf8aaca2f18c84575181b83cfde6
alvaro <alvaroaca18@gmail.com>
2025-10-22 19:31:45 +0200

    Ajustada configuración del favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,20 +9,12 @@ import { LanguageProvider } from '@/hooks/use-language';
 export const metadata: Metadata = {
   title: 'FrikiFigures',
   description: 'Your one-stop shop for geeky figures!',
-  icons: [
-    {
-      rel: 'icon',
+  icons: {
+    icon: [{
       url: '/favicon.png',
       type: 'image/png',
-      sizes: '32x32'
-    },
-    {
-      rel: 'apple-touch-icon',
-      url: '/favicon.png',
-      type: 'image/png',
-      sizes: '180x180'
-    }
-  ]
+    }],
+  }
 };
 
 export default function RootLayout({
@@ -33,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Orbitron:wght@400;700;900&display=swap" rel="stylesheet" />
